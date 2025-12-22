ef4ac28adb8a239313412836fa9b4801f921b9b5
Leandro Luna <135192663+LeandroLuna22@users.noreply.github.com>
2025-12-21 21:59:37 -0300

    no changes

--- public/js/criar-nota.js
+++ public/js/criar-nota.js
@@ -1,28 +1,3 @@
 /*Envia a nota para o backend usando fetch.
 Usa FormData para permitir envio de imagem.*/
 
-// criar-nota.js
-document.getElementById('form-nota').addEventListener('submit', async (e) => {
-    e.preventDefault();
-
-    const form = document.getElementById('form-nota');
-    const formData = new FormData(form);
-
-    try {
-        const resposta = await fetch('/criar-nota', {
-            method: 'POST',
-            body: formData
-        });
-
-        if (!resposta.ok) {
-            alert('Erro ao enviar a nota');
-            return;
-        }
-
-        alert('Nota criada com sucesso!');
-        form.reset();
-
-    } catch (erro) {
-        alert('Erro de conexão com o servidor');
-    }
-});
