5486e9a6ac8873c299a2a1d1c5f2e08905961770
Leandro Luna <135192663+LeandroLuna22@users.noreply.github.com>
2025-12-23 20:30:55 -0300

    LinuxServer
    Servidor Linux funcionando

--- server.js
+++ server.js
@@ -283,7 +283,7 @@ app.post('/logout', (req, res) => {
 // 🚀 START
 // ======================================================
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
     console.log(`Servidor rodando na porta ${PORT}`);
 });
 
